3d8223113eb175098c7d2ace1651d2dd4936d5f8
Faugnom1 <128656243+Faugnom1@users.noreply.github.com>
2023-06-07 20:28:55 -0400

    Create letconst.js

---
+++ letconst.js
@@ -0,0 +1,9 @@
+const PI = 3.14
+
+//You can reassign and redeclariw ith var but cannot redeclair with let.
+
+//Same as var and let but you cannot reassign with const.
+
+//You can reassign with let but you cannot reassign or redeclare with const.
+
+//Hoisting is varables listed to the top when they are declared
